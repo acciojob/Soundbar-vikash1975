8d795f8681da737bddb3f42de1164b4c9f906b83
vikash1975 <167326024+vikash1975@users.noreply.github.com>
2025-09-01 20:58:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+
+const sounds=["boo", "applause", "gasp","tada", "victory"];
+sounds.forEach((sound)=>{
+	let btn=document.createElement("button");
+	btn.classList.add("btn");
+	btn.innerText=sound;
+
+	btn.addEventListener("click",()=>{
+		stopSongs();
+		const audio=document.getElementById(sound);
+		audio.play();
+		
+	})
+	document.getElementById("buttons").appendChild(btn);
+});
+function stopSongs(){
+	sounds.forEach((sound)=>{
+		const audio=document.getElementById(sound);
+		audio.pause();
+		audio.currentTime=0;
+	});
+}
